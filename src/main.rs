d6346e3e4fccaf0f18ea22f4ca0d53645f44520a
flooijdt <flooijdt@localhost.localdomain>
2022-11-22 09:53:06 -0300

    made it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
-    println!("Hello, world!");
-    pp
+    println!("{:?}", is_prime(8));
+}
+fn is_prime(x: i64) -> bool {
+    // code here
+    for i in 1..=x {
+        println!("{:?}", i);
+    }
+    true
 }
